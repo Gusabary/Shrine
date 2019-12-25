2ebcaf28dad52f9bca8345c5e9838c7a85143912
unknown <Gusabary@126.com>
2019-12-25 11:46:42 +0800

    refactor(eight-diagram): add EightDiagram class

---
+++ Shrine/Shrine/eight-diagram/eight-diagram.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+
+#include "../shader.h"
+
+//using namespace std;
+
+float sina = glm::sin(22.5f);
+float cosa = glm::cos(22.5f);
+float d = 0.08f;
+float e = 1.0f / (2.0f * glm::cos(22.5f));
+float sine = e * sina;
+float cose = e * cosa;
+float eightDiagram[] = {
+	-sina - d, cosa, 0.0f, -sine + d + 0.5f, cose + 0.5f,
+	sina + d, cosa, 0.0f, sine - d + 0.5f, cose + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	sina + d, cosa, 0.0f, sine - d + 0.5f, cose + 0.5f,
+	cosa, sina + d, 0.0f, cose + 0.5f, sine - d + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	cosa, sina + d, 0.0f, cose + 0.5f, sine - d + 0.5f,
+	cosa, -sina - d, 0.0f, cose + 0.5f, -sine + d + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	cosa, -sina - d, 0.0f, cose + 0.5f, -sine + d + 0.5f,
+	sina + d, -cosa, 0.0f, sine - d + 0.5f, -cose + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	sina + d, -cosa, 0.0f, sine - d + 0.5f, -cose + 0.5f,
+	-sina - d, -cosa, 0.0f, -sine + d + 0.5f, -cose + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	-sina - d, -cosa, 0.0f, -sine + d + 0.5f, -cose + 0.5f,
+	-cosa, -sina - d, 0.0f, -cose + 0.5f, -sine + d + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	-cosa, -sina - d, 0.0f, -cose + 0.5f, -sine + d + 0.5f,
+	-cosa, sina + d, 0.0f, -cose + 0.5f, sine - d + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+	-cosa, sina + d, 0.0f, -cose + 0.5f, sine - d + 0.5f,
+	-sina - d, cosa, 0.0f, -sine + d + 0.5f, cose + 0.5f,
+	0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+};
+
+class EightDiagram {
+public:
+	Shader eightDiagramShader;
+
+	EightDiagram()
+		: eightDiagramShader(Shader("eight-diagram/eight-diagram.vs", "eight-diagram/eight-diagram.fs")) {
+
+		// eightDiagram VAO
+		glGenVertexArrays(1, &edVAO);
+		glGenBuffers(1, &edVBO);
+		glBindVertexArray(edVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, edVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(eightDiagram), &eightDiagram, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+		// load textures
+		// -------------
+		edTexture = loadTexture("resources/textures/eight-diagram.jpg");
+
+		eightDiagramShader.use();
+		eightDiagramShader.setInt("texture1", 0);
+	}
+
+	void drawEightDiagram(glm::mat4 model, glm::mat4 view, glm::mat4 projection) {
+		eightDiagramShader.use();
+		//view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
+		eightDiagramShader.setMat4("model", model);
+		eightDiagramShader.setMat4("view", view);
+		eightDiagramShader.setMat4("projection", projection);
+		
+		glBindVertexArray(edVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, edTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 24);
+		glBindVertexArray(0);
+	}
+
+private:
+	// utility function for loading a 2D texture from file
+	// ---------------------------------------------------
+	unsigned int edVAO, edVBO;
+	unsigned int edTexture;
+
+	unsigned int loadTexture(char const * path)
+	{
+		unsigned int textureID;
+		glGenTextures(1, &textureID);
+
+		int width, height, nrComponents;
+		unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+		if (data)
+		{
+			GLenum format;
+			if (nrComponents == 1)
+				format = GL_RED;
+			else if (nrComponents == 3)
+				format = GL_RGB;
+			else if (nrComponents == 4)
+				format = GL_RGBA;
+
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Texture failed to load at path: " << path << std::endl;
+			stbi_image_free(data);
+		}
+
+		return textureID;
+	}
+};
--- Shrine/Shrine/main.cpp
+++ Shrine/Shrine/main.cpp
@@ -10,6 +10,7 @@
 #include "camera.h"
 #include "model.h"
 #include "skybox/skybox.h"
+#include "eight-diagram/eight-diagram.h"
 
 #include <iostream>
 
@@ -17,7 +18,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
-unsigned int loadTexture(const char *path);
 
 // settings
 const unsigned int SCR_WIDTH = 1280;
@@ -70,127 +70,9 @@ int main()
 	// configure global opengl state
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST);
-
-	// build and compile shaders
-	// -------------------------
-	Shader shader("cubemaps.vs", "cubemaps.fs");
-
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float cubeVertices[] = {
-		// positions          // texture Coords
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-	float sin = glm::sin(22.5f);
-	float cos = glm::cos(22.5f);
-	float d = 0.08f;
-	float e = 1.0f / (2.0f * glm::cos(22.5f));
-	float sine = e * sin;
-	float cose = e * cos;
-	float eightDiagram[] = {
-		-sin - d, cos, 0.0f, -sine + d + 0.5f, cose + 0.5f,
-		sin + d, cos, 0.0f, sine - d + 0.5f, cose + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		sin + d, cos, 0.0f, sine - d + 0.5f, cose + 0.5f,
-		cos, sin + d, 0.0f, cose + 0.5f, sine - d + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		cos, sin + d, 0.0f, cose + 0.5f, sine - d + 0.5f,
-		cos, -sin - d, 0.0f, cose + 0.5f, -sine + d + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		cos, -sin - d, 0.0f, cose + 0.5f, -sine + d + 0.5f,
-		sin + d, -cos, 0.0f, sine - d + 0.5f, -cose + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		sin + d, -cos, 0.0f, sine - d + 0.5f, -cose + 0.5f,
-		-sin - d, -cos, 0.0f, -sine + d + 0.5f, -cose + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		-sin - d, -cos, 0.0f, -sine + d + 0.5f, -cose + 0.5f,
-		-cos, -sin - d, 0.0f, -cose + 0.5f, -sine + d + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		-cos, -sin - d, 0.0f, -cose + 0.5f, -sine + d + 0.5f,
-		-cos, sin + d, 0.0f, -cose + 0.5f, sine - d + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-		-cos, sin + d, 0.0f, -cose + 0.5f, sine - d + 0.5f,
-		-sin - d, cos, 0.0f, -sine + d + 0.5f, cose + 0.5f,
-		0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
-	};
-
-	// eightDiagram VAO
-	unsigned int edVAO, edVBO;
-	glGenVertexArrays(1, &edVAO);
-	glGenBuffers(1, &edVBO);
-	glBindVertexArray(edVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, edVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(eightDiagram), &eightDiagram, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-
-	unsigned int edTexture = loadTexture("resources/textures/eight-diagram.jpg");
-
-	// cube VAO
-	unsigned int cubeVAO, cubeVBO;
-	glGenVertexArrays(1, &cubeVAO);
-	glGenBuffers(1, &cubeVBO);
-	glBindVertexArray(cubeVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-
+	
 	SkyBox skybox = SkyBox();
-
-	// load textures
-	// -------------
-	unsigned int cubeTexture = loadTexture("resources/textures/container.jpg");
-
-	// shader configuration
-	// --------------------
-	shader.use();
-	shader.setInt("texture1", 0);
+	EightDiagram eightDiagram = EightDiagram();
 
 	// render loop
 	// -----------
@@ -209,27 +91,12 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// draw scene as normal
-		shader.use();
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		shader.setMat4("model", model);
-		shader.setMat4("view", view);
-		shader.setMat4("projection", projection);
-
-		// cubes
-		//glBindVertexArray(cubeVAO);
-		//glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, cubeTexture);
-		//glDrawArrays(GL_TRIANGLES, 0, 36);
-		//glBindVertexArray(0);
 
 		// eight-diagram
-		glBindVertexArray(edVAO);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, edTexture);
-		glDrawArrays(GL_TRIANGLES, 0, 24);
-		glBindVertexArray(0);
+		eightDiagram.drawEightDiagram(model, view, projection);
 
 		// skybox
 		view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
@@ -243,9 +110,9 @@ int main()
 
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &cubeVAO);
+	// glDeleteVertexArrays(1, &cubeVAO);
 	// glDeleteVertexArrays(1, &skyboxVAO);
-	glDeleteBuffers(1, &cubeVBO);
+	// glDeleteBuffers(1, &cubeVBO);
 	// glDeleteBuffers(1, &skyboxVAO);
 
 	glfwTerminate();
@@ -303,43 +170,4 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	camera.ProcessMouseScroll(yoffset);
-}
-
-// utility function for loading a 2D texture from file
-// ---------------------------------------------------
-unsigned int loadTexture(char const * path)
-{
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
-
-	int width, height, nrComponents;
-	unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
-	if (data)
-	{
-		GLenum format;
-		if (nrComponents == 1)
-			format = GL_RED;
-		else if (nrComponents == 3)
-			format = GL_RGB;
-		else if (nrComponents == 4)
-			format = GL_RGBA;
-
-		glBindTexture(GL_TEXTURE_2D, textureID);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		stbi_image_free(data);
-	}
-	else
-	{
-		std::cout << "Texture failed to load at path: " << path << std::endl;
-		stbi_image_free(data);
-	}
-
-	return textureID;
 }
