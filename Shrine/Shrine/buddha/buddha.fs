d0139093d6ebf63c2cb41f61b45484c7272dc2a0
unknown <Gusabary@126.com>
2019-12-28 19:44:30 +0800

    feat(buddha): add light effect of buddha

--- Shrine/Shrine/buddha/buddha.fs
+++ Shrine/Shrine/buddha/buddha.fs
@@ -1,21 +1,78 @@
 #version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+	vec3 color;
+
+	vec3 direction;
+    float cutOff;
+    float outerCutOff;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
 out vec4 FragColor;
 
-in vec2 TexCoords;
-in vec3 FragPos;
-
-uniform sampler2D texture_diffuse1;
-uniform vec3 cameraPos;
+uniform Light light;
+uniform Material material;
+uniform vec3 viewPos;
 
 void main()
-{    
-	float fogDistance = sqrt(pow(cameraPos.x - FragPos.x, 2) + pow(cameraPos.z - FragPos.z, 2));
+{
+    // ambient
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb * light.color;
+    
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb * light.color;  
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb * light.color;  
+
+	// spotlight (soft edges)
+    float theta = dot(lightDir, normalize(-light.direction)); 
+    float epsilon = (light.cutOff - light.outerCutOff);
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    diffuse  *= intensity;
+    specular *= intensity;
+    
+    // attenuation
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    ambient  *= attenuation; 
+    diffuse   *= attenuation;
+    specular *= attenuation;   
+        
+    vec3 result = ambient + diffuse + specular;
+
+	float fogDistance = sqrt(pow(viewPos.x - FragPos.x, 2) + pow(viewPos.z - FragPos.z, 2));
 	float fogMax = 40.0f;
 	float fogMin = 5.0f;
 	float fogFactor = (fogMax - fogDistance) / (fogMax - fogMin);
 	fogFactor = clamp(fogFactor, 0.3f, 1.0f);
 	vec4 fogColor = vec4(0.8f, 0.8f, 0.8f, 1.0f);
-    FragColor = mix(fogColor, texture(texture_diffuse1, TexCoords), fogFactor);
+    FragColor = mix(fogColor, vec4(result, 1.0), fogFactor);
 
-    //FragColor = texture(texture_diffuse1, TexCoords);
+   // FragColor = vec4(result, 1.0);
 }
--- Shrine/Shrine/buddha/buddha.h
+++ Shrine/Shrine/buddha/buddha.h
@@ -21,6 +21,26 @@ public:
 		shader.setMat4("projection", projection);
 		shader.setMat4("view", view);
 		shader.setMat4("model", model);
+
+		shader.setInt("material.diffuse", 0);
+		shader.setInt("material.specular", 1);
+		shader.setFloat("material.shininess", 32.0f);
+		shader.setVec3("light.position", camera.Position);
+		shader.setVec3("light.color", 1.0f, 1.0f, 1.0f);
+		shader.setVec3("light.direction", camera.Front);
+		shader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		shader.setFloat("light.outerCutOff", glm::cos(glm::radians(30.0f)));
+		shader.setVec3("viewPos", camera.Position);
+		float ambientValue = 0.3f;
+		float diffuseValue = camera.IsLightOn ? 0.8f : 0.0f;
+		float specularValue = camera.IsLightOn ? 1.0f : 0.0f;
+		shader.setVec3("light.ambient", ambientValue, ambientValue, ambientValue);
+		shader.setVec3("light.diffuse", diffuseValue, diffuseValue, diffuseValue);
+		shader.setVec3("light.specular", specularValue, specularValue, specularValue);
+		shader.setFloat("light.constant", 1.0f);
+		shader.setFloat("light.linear", 0.09f);
+		shader.setFloat("light.quadratic", 0.032f);
+
 		buddhaModel.Draw(shader);
 	}
 
