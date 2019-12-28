12f070cdd3f58eb126c89c5aedc8c1f5c9e9758f
unknown <Gusabary@126.com>
2019-12-28 13:27:23 +0800

    style(ground): repeat texture

--- Shrine/Shrine/ground/ground.h
+++ Shrine/Shrine/ground/ground.h
@@ -10,13 +10,13 @@
 
 const float groundSize = 100.0f;
 const float groundVertices[] = {
-	groundSize, 0.0f, -groundSize, 1.0f, 1.0f,   // 右上
-	-groundSize, 0.0f, -groundSize, 0.0f, 1.0f,    // 左上
+	groundSize, 0.0f, -groundSize, 5.0f, 5.0f,   // 右上
+	-groundSize, 0.0f, -groundSize, 0.0f, 5.0f,    // 左上
 	-groundSize, 0.0f, groundSize, 0.0f, 0.0f,   // 左下
 
-	groundSize, 0.0f, groundSize, 1.0f, 0.0f,   // 右下
+	groundSize, 0.0f, groundSize, 5.0f, 0.0f,   // 右下
 	-groundSize, 0.0f, groundSize, 0.0f, 0.0f,   // 左下
-	groundSize, 0.0f, -groundSize, 1.0f, 1.0f,   // 右上
+	groundSize, 0.0f, -groundSize, 5.0f, 5.0f,   // 右上
 };
 
 class Ground {
@@ -87,8 +87,8 @@ private:
 			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 			glGenerateMipmap(GL_TEXTURE_2D);
 
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
