5b5541232124add365108ed9a16eebd538b11cd9
unknown <Gusabary@126.com>
2020-01-09 13:45:33 +0800

    style: change title to Shrine

--- Shrine/Shrine/main.cpp
+++ Shrine/Shrine/main.cpp
@@ -61,7 +61,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Shrine", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
